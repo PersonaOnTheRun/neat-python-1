00e794d9f52d4cb72443db2eaa99b4f94f67e027
PersonaOnTheRun <cammilligan@Camerons-MacBook-Pro.local>
2017-02-10 21:39:32 -0500

    final

--- neat-pythinglearning.py
+++ neat-pythinglearning.py
@@ -38,7 +38,7 @@ for j in range(60000):
     l1 = nonlin(np.dot(l0,syn0))
     l2 = nonlin(np.dot(l1,syn1))
     
-    l2_error = y - 12
+    l2_error = y - l2
     
     if(j % 10000) == 0:
         print("Error:"+ str(np.mean(np.abs(l2_error))))
@@ -48,11 +48,10 @@ for j in range(60000):
     l1_error = l2_delta.dot(syn1.T)
     
     l1_delta = l1_error * nonlin(l1, deriv=True)
-    
-    
+       
     syn1 += l1.T.dot(l2_delta)
     syn0 += l0.T.dot(l1_delta)
     
 print("Output after trianing")
-print("l2")
+print(l2)
 
